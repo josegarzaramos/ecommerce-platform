021dfe69ea2630153df292aa0814fbd445b59680
Jose Luis Garza <josegarzaramos@gmail.com>
2025-06-10 11:19:14 -0500

    feat(products): create dynamic product detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import prisma from "@/lib/prisma";
 import type { Product } from "@prisma/client";
 
@@ -19,32 +20,34 @@ export default async function HomePage() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {products.map((product) => (
-          <div
-            key={product.id}
-            className="border rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200"
-          >
-            {product.images.length > 0 && (
-              <div className="w-full h-48 bg-gray-200 flex items-center justify-center overflow-hidden rounded-t-lg">
-                <img
-                  src={product.images[0]}
-                  alt={product.name}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            )}
+          <Link key={product.id} href={`/products/${product.id}`}>
+            <div
+              key={product.id}
+              className="border rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200"
+            >
+              {product.images.length > 0 && (
+                <div className="w-full h-48 bg-gray-200 flex items-center justify-center overflow-hidden rounded-t-lg">
+                  <img
+                    src={product.images[0]}
+                    alt={product.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              )}
 
-            <div className="p-4">
-              <h2 className="text-xl font-semibold mb-2 truncate">
-                {product.name}
-              </h2>
-              <p className="text-gray-600 mb-4 text-sm line-clamp-2">
-                {product.description}
-              </p>
-              <p className="text-2xl font-bold text-right">
-                ${product.price.toNumber().toFixed(2)}
-              </p>
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mb-2 truncate">
+                  {product.name}
+                </h2>
+                <p className="text-gray-600 mb-4 text-sm line-clamp-2">
+                  {product.description}
+                </p>
+                <p className="text-2xl font-bold text-right">
+                  ${product.price.toNumber().toFixed(2)}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </main>
